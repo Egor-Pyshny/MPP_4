fa3446ad22014a779835731e34e2c27d7037cc94
Egor251 <egorpyshny@gmail.com>
2024-03-02 15:11:04 +0300

    rewrite using dataflow

--- MPP_4/Program.cs
+++ MPP_4/Program.cs
@@ -1,107 +1,66 @@
 ﻿using System.Collections.Concurrent;
 using System.Data;
 using System.Runtime.Serialization;
+using System.Threading.Tasks.Dataflow;
 using TestGenerator.TestGenerators;
 
-BlockingCollection<string> sourceFilesQueue = new BlockingCollection<string>();
-BlockingCollection<string> testsTextQueue = new BlockingCollection<string>();
-BlockingCollection<Task> tasksCollection = new BlockingCollection<Task>();
-List<Task> tasks = new List<Task>();
-
 Generator g = new Generator();
+string[] pathes = new string[] { "C:\\Users\\user\\Source\\Repos\\Egor-Pyshny\\MPP_4\\MPP_4\\Program.cs" };
+var buffer = new BufferBlock<string>();
 
-
-void StartGenerating(List<string> filePathes, List<int> parallelRestrictions) {
-    Task loadingTask = LoadFilesAsync(filePathes, parallelRestrictions[0]);
-    Task GeneratingTests = GenerateAll(parallelRestrictions[1]);
-}
-
-async Task LoadFilesAsync(List<string> filePathes, int maxP)
+var readerOptions = new ExecutionDataflowBlockOptions
 {
-    await Task.Run(() =>
-    {
-        SemaphoreSlim semaphore = new SemaphoreSlim(maxP);
-        Parallel.ForEach(filePathes, file =>
-        {
-            semaphore.Wait();
-            try
-            {
-                sourceFilesQueue.Add(file);
-            }
-            finally
-            {
-                semaphore.Release();
-            }
-        });
-    });
-    sourceFilesQueue.CompleteAdding();
+    MaxDegreeOfParallelism = 4,
+    BoundedCapacity = 100
+};
+var reader = new TransformBlock<string, string>(readerF, readerOptions);
+
+var generatorOptions = new ExecutionDataflowBlockOptions
+{
+    MaxDegreeOfParallelism = 4,
+    BoundedCapacity = 100
+};
+var generator = new TransformBlock<string, ConcurrentDictionary<string, string>>(g.GenerateTestClasses, generatorOptions);
+
+var writerOptions = new ExecutionDataflowBlockOptions
+{
+    MaxDegreeOfParallelism = 4,
+    BoundedCapacity = 100
+};
+var writer = new ActionBlock<ConcurrentDictionary<string, string>>(writerF, generatorOptions);
+
+buffer.LinkTo(reader);
+reader.LinkTo(generator);
+generator.LinkTo(writer);
+
+foreach (var item in pathes)
+{
+    buffer.Post(item);
 }
 
-async Task GenerateAll(int maxP) {
-    await Task.Run(() =>
+writer.Completion.Wait();
+
+async Task<string> readerF(string path) {
+    return await File.ReadAllTextAsync(path);
+}
+
+
+async Task writerF(ConcurrentDictionary<string, string> dict)
+{
+    var dir = Directory.CreateDirectory("C:\\Users\\user\\Desktop\\test");
+    foreach (var item in dict)
     {
-        while (!sourceFilesQueue.IsCompleted || sourceFilesQueue.Count > 0)
-        {
-            if (tasks.Count > maxP)
-            {
-                var file = sourceFilesQueue.Take();
-                Task task = g.GenerateTestClasses(file);
-                task.ContinueWith(t => {
-                    tasks.Remove(t);
-                    //testsTextQueue.
-                    });
-                tasks.Add(task);
-            }
+        if (dir.EnumerateFiles().Where(f => f.Name == item.Key).ToList().Count == 1) {
+            throw new Exception();
         }
-    });
-
-}
-
-
-
-ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
-
-async Task Main()
-{
-    // Запуск асинхронного метода, который добавляет элементы в очередь
-    Task consumerTask = ConsumeAsync();
-    Task producerTask = ProduceAsync();
-
-    // Запуск асинхронного метода, который берет элементы из очереди
-    
-
-    // Ожидание завершения обоих задач
-    await Task.WhenAll(producerTask, consumerTask);
-
-    Console.WriteLine("Все элементы добавлены и извлечены из очереди.");
-}
-
-async Task ProduceAsync()
-{
-    for (int i = 0; i < 10; i++)
-    {
-        await Task.Delay(1);
-        queue.Enqueue(i);
-        Console.WriteLine($"Элемент {i} добавлен в очередь.");
+        var f = File.Create("C:\\Users\\user\\Desktop\\test\\txt.txt");
+        StreamWriter stream = new StreamWriter(f);
+        await stream.WriteLineAsync(item.Value);
+        await stream.FlushAsync();
+        stream.Close();
     }
+
 }
-
-async Task ConsumeAsync()
-{
-    while (true)
-    {
-
-        await Task.Delay(1);
-        if (queue.TryDequeue(out int item))
-        {
-            Console.WriteLine($"Элемент {item} извлечен из очереди.");
-        }
-
-    }
-}
-
-
-await Main();
 
 public class Class1 {
     public int Test(int a, int b, int c) { return 5; }
