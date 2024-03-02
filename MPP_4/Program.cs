bc017b38f6a6fd8bdb3054b42340a53fb3b6faa6
Egor251 <egorpyshny@gmail.com>
2024-03-02 17:34:34 +0300

    add tests

--- MPP_4/Program.cs
+++ MPP_4/Program.cs
@@ -4,56 +4,23 @@ using System.Runtime.Serialization;
 using System.Threading.Tasks.Dataflow;
 using TestGenerator.TestGenerators;
 
-Generator g = new Generator();
-string[] pathes = new string[] { "C:\\Users\\user\\Source\\Repos\\Egor-Pyshny\\MPP_4\\MPP_4\\Program.cs" };
-var buffer = new BufferBlock<string>();
 
-var readerOptions = new ExecutionDataflowBlockOptions
-{
-    MaxDegreeOfParallelism = 4,
-    BoundedCapacity = 100
-};
-var reader = new TransformBlock<string, string>(readerF, readerOptions);
-
-var generatorOptions = new ExecutionDataflowBlockOptions
-{
-    MaxDegreeOfParallelism = 4,
-    BoundedCapacity = 100
-};
-var generator = new TransformBlock<string, ConcurrentDictionary<string, string>>(g.GenerateTestClasses, generatorOptions);
-
-var writerOptions = new ExecutionDataflowBlockOptions
-{
-    MaxDegreeOfParallelism = 4,
-    BoundedCapacity = 100
-};
-var writer = new ActionBlock<ConcurrentDictionary<string, string>>(writerF, generatorOptions);
-
-buffer.LinkTo(reader);
-reader.LinkTo(generator);
-generator.LinkTo(writer);
-
-foreach (var item in pathes)
-{
-    buffer.Post(item);
-}
-
-writer.Completion.Wait();
-
-async Task<string> readerF(string path) {
+async Task<string> Read(string path) {
     return await File.ReadAllTextAsync(path);
 }
 
-
-async Task writerF(ConcurrentDictionary<string, string> dict)
+async Task Write(ConcurrentDictionary<string, string> dict)
 {
-    var dir = Directory.CreateDirectory("C:\\Users\\user\\Desktop\\test");
+    var dir = Directory.CreateDirectory("TESTS");
     foreach (var item in dict)
     {
         if (dir.EnumerateFiles().Where(f => f.Name == item.Key).ToList().Count == 1) {
             throw new Exception();
         }
-        var f = File.Create("C:\\Users\\user\\Desktop\\test\\txt.txt");
+        int copyNumber = 1;
+        string filename = $"TESTS\\{item.Key}.cs";
+        while(File.Exists(filename)) filename = $"TESTS\\{item.Key}({copyNumber++}).cs";
+        var f = File.Create(filename);
         StreamWriter stream = new StreamWriter(f);
         await stream.WriteLineAsync(item.Value);
         await stream.FlushAsync();
@@ -62,9 +29,55 @@ async Task writerF(ConcurrentDictionary<string, string> dict)
 
 }
 
+void GenerateTestClasses(string[] pathes, int[] MaxDegreesOfParallelism)
+{ 
+    Generator g = new Generator();
+    var buffer = new BufferBlock<string>();
+
+    var readerOptions = new ExecutionDataflowBlockOptions
+    {
+        MaxDegreeOfParallelism = MaxDegreesOfParallelism[0],
+    };
+    var reader = new TransformBlock<string, string>(Read, readerOptions);
+
+    var generatorOptions = new ExecutionDataflowBlockOptions
+    {
+        MaxDegreeOfParallelism = MaxDegreesOfParallelism[1],
+    };
+    var generator = new TransformBlock<string, ConcurrentDictionary<string, string>>(g.GenerateTestClasses, generatorOptions);
+
+    var writerOptions = new ExecutionDataflowBlockOptions
+    {
+        MaxDegreeOfParallelism = MaxDegreesOfParallelism[2],
+    };
+    var writer = new ActionBlock<ConcurrentDictionary<string, string>>(Write, generatorOptions);
+
+    buffer.LinkTo(reader);
+    reader.LinkTo(generator);
+    generator.LinkTo(writer);
+
+    buffer.Completion.ContinueWith(task => reader.Complete());
+    reader.Completion.ContinueWith(task => generator.Complete());
+    generator.Completion.ContinueWith(task => writer.Complete());
+
+    foreach (var item in pathes)
+    {
+        buffer.Post(item);
+    }
+    buffer.Complete();
+
+    writer.Completion.Wait();
+}
+
+int[] MaxDegreesOfParallelism = new int[] { 4, 4, 4 };
+string[] pathes = new string[] { 
+    "C:\\Users\\Пользователь\\source\\repos\\MPP_4\\MPP_4\\Program.cs" 
+};
+GenerateTestClasses(pathes, MaxDegreesOfParallelism);
+
 public class Class1 {
     public int Test(int a, int b, int c) { return 5; }
-    public void Test(int a, int b) {}
+    public void Test(IDataReader a, int b) {}
     public void Test1() { }
     public void Test2() { }
     private void Test3() { }
---
+++ Tests/HelperClasses/HelperClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace Tests.HelperClasses
+{
+    public class Class1
+    {
+        public int Test(IDataReader a, int b, int c) { return 5; }
+        public void Test(int b) { }
+        public Class1(IDataReader a, ICloneable o, string c) { }
+    }
+}
